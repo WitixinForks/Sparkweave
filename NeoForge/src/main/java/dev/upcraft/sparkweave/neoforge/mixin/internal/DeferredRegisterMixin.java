a131f476efc5cf84ea6fd17c9cce8ac1243e26aa
Up <upcraftedlp@gmail.com>
2024-02-16 14:02:03 +0100

    fix neo deferred registers

--- NeoForge/src/main/java/dev/upcraft/sparkweave/neoforge/mixin/internal/DeferredRegisterMixin.java
+++ NeoForge/src/main/java/dev/upcraft/sparkweave/neoforge/mixin/internal/DeferredRegisterMixin.java
@@ -7,6 +7,8 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.spongepowered.asm.mixin.Implements;
+import org.spongepowered.asm.mixin.Interface;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Invoker;
@@ -15,36 +17,41 @@ import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-@SuppressWarnings("AddedMixinMembersNamePattern")
+@Implements(@Interface(iface = RegistryHandler.class, prefix = "handler$"))
 @Mixin(DeferredRegister.class)
-public abstract class DeferredRegisterMixin<T> implements RegistryHandler<T> {
+public abstract class DeferredRegisterMixin<T> {
 
 	@Shadow
 	public abstract Collection<DeferredHolder<T, ? extends T>> getEntries();
 
+	@Shadow
+	public abstract <I extends T> DeferredHolder<T, I> register(String name, Supplier<? extends I> sup);
+
 	@SuppressWarnings("unchecked")
-	@Override
-	public Map<ResourceLocation, RegistrySupplier<? extends T>> values() {
+	public <S extends T> RegistrySupplier<S> handler$register(String name, Supplier<S> factory) {
+		return (RegistrySupplier<S>) this.register(name, factory);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Map<ResourceLocation, RegistrySupplier<? extends T>> handler$values() {
 		return (Map<ResourceLocation, RegistrySupplier<? extends T>>) (Object) this.getEntries().stream().collect(Collectors.toMap(DeferredHolder::getId, UnaryOperator.identity()));
 	}
 
 	@SuppressWarnings("unchecked")
-	@Override
-	public List<RegistrySupplier<? extends T>> getEntriesOrdered() {
+	public List<RegistrySupplier<? extends T>> handler$getEntriesOrdered() {
 		return (List<RegistrySupplier<? extends T>>) (Object) this.getEntries().stream().sorted(Comparator.comparing(DeferredHolder::getKey, Comparator.naturalOrder())).toList();
 	}
 
 	@SuppressWarnings("unchecked")
-	@Override
-	public Stream<RegistrySupplier<? extends T>> stream() {
+	public Stream<RegistrySupplier<? extends T>> handler$stream() {
 		return (Stream<RegistrySupplier<? extends T>>) (Object) this.getEntries().stream();
 	}
 
 	@Invoker("getRegistryKey")
-	@Override
-	public abstract ResourceKey<Registry<T>> registry();
+	public abstract ResourceKey<Registry<T>> handler$registry();
 }
