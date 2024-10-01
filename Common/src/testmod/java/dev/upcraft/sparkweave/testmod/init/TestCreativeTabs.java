511d92ec7ab65524c27b53c823fc165f67a0eb94
Up <upcraftedlp@gmail.com>
2024-10-02 01:07:21 +0200

    make testmod not use deprecated methods

--- Common/src/testmod/java/dev/upcraft/sparkweave/testmod/init/TestCreativeTabs.java
+++ Common/src/testmod/java/dev/upcraft/sparkweave/testmod/init/TestCreativeTabs.java
@@ -5,15 +5,13 @@ import dev.upcraft.sparkweave.api.registry.RegistryHandler;
 import dev.upcraft.sparkweave.api.registry.RegistrySupplier;
 import dev.upcraft.sparkweave.testmod.SparkweaveTestmod;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.CreativeModeTab;
 
 public class TestCreativeTabs {
 
 	public static final RegistryHandler<CreativeModeTab> TABS = RegistryHandler.create(Registries.CREATIVE_MODE_TAB, SparkweaveTestmod.MODID);
 
-	public static final RegistrySupplier<CreativeModeTab> ITEMS = TABS.register("items", () -> CreativeTabHelper.newBuilder()
-		.title(Component.translatable("itemGroup.sparkweave_testmod.items"))
+	public static final RegistrySupplier<CreativeModeTab> ITEMS = TABS.register("items", () -> CreativeTabHelper.newBuilder(SparkweaveTestmod.id("items"))
 		.icon(() -> TestItems.TEST_ITEM.get().getDefaultInstance())
 		.displayItems((itemDisplayParameters, output) -> CreativeTabHelper.addRegistryEntries(itemDisplayParameters, output, TestItems.ITEMS))
 		.build()
