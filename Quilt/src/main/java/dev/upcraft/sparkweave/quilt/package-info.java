43de8ac0cfbe6d11347409a87082309505d6b7ca
Up <upcraftedlp@gmail.com>
2024-10-28 06:31:46 +0100

    remove quilt submodule

--- Quilt/src/main/java/dev/upcraft/sparkweave/quilt/package-info.java
+++
@@ -1,5 +0,0 @@
-@Mod.Context(SparkweaveMod.MODID)
-package dev.upcraft.sparkweave.quilt;
-
-import dev.upcraft.sparkweave.SparkweaveMod;
-import dev.upcraft.sparkweave.api.annotation.Mod;
--- Quilt/src/testmod/java/dev/upcraft/sparkweave/testmod/quilt/package-info.java
+++
@@ -1,5 +0,0 @@
-@Mod.Context(SparkweaveTestmod.MODID)
-package dev.upcraft.sparkweave.testmod.quilt;
-
-import dev.upcraft.sparkweave.api.annotation.Mod;
-import dev.upcraft.sparkweave.testmod.SparkweaveTestmod;
